3428ce8cb445de50da1e8c88ae2b56bc3c4a1bf9
Synthetic6969 <69759521+Synthetic6969@users.noreply.github.com>
2021-01-28 13:18:29 +0000

    Create randomString.lua

---
+++ randomString.lua
@@ -0,0 +1,22 @@
+function generateRandomString(size, options)
+    if not options then options = {} end
+    local ignoreChars = options.ignoreChars or {}
+    local minChar = options.minChar or 0
+    local maxChar = options.maxChar or 200
+    
+    local s, i = "", 0
+    while i < size do
+        local ignoreChar
+        local char = string.char(math.random(minChar or 1, maxChar or 200))
+        for _, v in next, ignoreChars do
+            if char == v then
+                ignoreChar = true
+            end
+        end
+        if not ignoreChar then
+            i = i + 1
+            s = s..char
+        end
+    end
+    return s
+end
