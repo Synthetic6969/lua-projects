11b3654434d1abb77f33dc591ed938b0993991db
Synthetic6969 <69759521+Synthetic6969@users.noreply.github.com>
2021-01-30 12:57:05 +0000

    Update tableToString.lua

--- tableToString.lua
+++ tableToString.lua
@@ -6,9 +6,9 @@ function concatTable(t)
         tabs = tabs + 1
         for i, v in pairs(t) do
             if type(i) == "string" then
-                s = s..string.rep("\t", tabs).."['"..i.."'] = "
+                s = s..string.rep("    ", tabs).."['"..i.."'] = "
             else
-                s = s..string.rep("\t", tabs).."["..i.."] = "
+                s = s..string.rep("    ", tabs).."["..tostring(i).."] = "
             end
             
             if type(v) == "table" then
@@ -17,16 +17,16 @@ function concatTable(t)
                 local vRemoved
                 vRemoved = string.gsub(v, "\n", "\\n")
                 s = s.."'"..vRemoved.."';".."\n"
+            elseif type(v) == "userdata" and typeof(v) == "Instance" then
+                s = s..v:GetFullName()..";\n"
             else
                 s = s..tostring(v)..";\n"
             end
         end
         tabs = tabs - 1
-        s = s..string.rep("\t", tabs).."};\n"
+        s = s..string.rep("    ", tabs).."};\n"
         return string.sub(s, 1, string.len(s)-2)
     end
     
     return searchTable(t)
 end
-
-return concatTable
